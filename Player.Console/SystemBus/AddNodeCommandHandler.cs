9735131f4a783fc4335bfbeeec6f685b5b18350a
Ahmed Bodiwala <ahmed_bodi@msn.com>
2020-05-30 00:51:36 +0100

    [SystemBus] Create AddNodeCommand + Dummy Handler

---
+++ Player.Console/SystemBus/AddNodeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using SystemBus.Commands;
+
+namespace NetworkScanner.SystemBus
+{
+    public class AddNodeCommandHandler : IHandleMessages<AddNodeCommand>
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public async Task Handle(AddNodeCommand message, IMessageHandlerContext context)
+        {
+            Logger.Info($"Received Add Node Command, Node = {message.Address}:{message.Port}");
+
+        }
+    }
+}
---
+++ SystemBus/Commands/AddNodeCommand.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+namespace SystemBus.Commands
+{
+    public class AddNodeCommand : ICommand
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+    }
+}
